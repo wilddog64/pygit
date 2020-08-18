ee25ee776c7a988d7808a5d16b0f7a3c815e4947
CK <chengkai.liang@bigfishgames.com>
2020-08-18 12:58:23 -0700

    comment each section of codes

--- pygit/cli/main.py
+++ pygit/cli/main.py
@@ -5,18 +5,24 @@ from pygit.utils import get_current_execution_path
 from pygit.utils import add_bool_argument
 
 def git():
+    # create an argparse object
     optParser = argparse.ArgumentParser(prog='git',
             description='python git',
             formatter_class=argparse.ArgumentDefaultsHelpFormatter,
             argument_default=argparse.SUPPRESS)
+
+    # setup up command line arguments
     optParser.add_argument('--git-url', '-g', default='https://github.com/bigfishgames', dest='git_url')
     optParser.add_argument('--repo-path', '-p', default='/tmp', dest='repo_path')
     optParser.add_argument('--repo-name', '-n', default='', dest='repo_name')
     optParser.add_argument('--branch-name', '-b', default='deploy_branch', dest='branch_name')
     optParser.add_argument('--commit', '-c', default='', dest='commit')
     add_bool_argument(optParser, 'removerepo', default=True)
+
+    # parse command line options
     args = optParser.parse_args()
 
+    # assign command line options to their variables
     git_url = args.git_url
     repo_path = args.repo_path
     repo_name = args.repo_name
@@ -24,6 +30,7 @@ def git():
     removerepo = args.removerepo
     commit = args.commit
 
+    # print out variables
     print('git url %s' % (git_url))
     print('repo path: %s' % (repo_path))
     print('repo name: %s' % (repo_name))
@@ -34,14 +41,22 @@ def git():
     print('repo url %s' % (repo_url,))
 
     repo = "%s/%s" % (repo_path, repo_name)
+
+    # clone repo to local if it is not exist
     Git.clone_repo_to_local(
             git_url=repo_url,
             repo_path=repo_path,
             app_name=repo_name,
             force_remove_repo=removerepo
             )
+
+    # pull from upstream
     Git.pull(repoPath=repo)
+
+    # remove all the untracked files
     Git.remove_repo_untrack_files(repo_path=repo)
+
+    # delete branch if it exists then checkout it again
     if Git.branch_exists(branch_name=branch_name, repo_path=repo):
         branch(branch_name, _cwd=repo, D=True)
     checkout(commit, b=branch_name,  _cwd=repo)
